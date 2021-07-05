cab5f1b1f8504a66a0f5fc8d38fa64822ef5c05e
AbbuTurab <syed.abbuturab@fissionlabs.com>
2021-07-05 11:08:47 +0530

    eliminated extras

--- app.js
+++ app.js
@@ -5,30 +5,6 @@ const db = require('./config/config')
 const userRouter = require('./api/users/user.router');
 
 app.use(express.json());
-// app.get('/api', (req, res) => {
-//   res.send({
-//     success: 1,
-//     message: 'This API is working',
-//   });
-// });
-
-// app.get('/createdb', (req, res) => {
-//   let sql = 'CREATE DATABASE registration';
-//   db.query(sql, (err, result) => {
-//     if (err) throw err;
-//     console.log(result);
-//     res.send('Database created...');
-//   });
-// });
-// //firstName, lastName, gender, email, password
-// app.get('/createusertable', (req, res) => {
-//   let sql = 'CREATE TABLE registration( id int AUTO_INCREMENT, firstName VARCHAR(55), lastName VARCHAR(55), gender VARCHAR(15), email VARCHAR(55), password VARCHAR(55), PRIMARY KEY (id))';
-//   db.query(sql, (err, result) => {
-//     if (err) throw err;
-//     console.log(result);
-//     res.send('registration table created');
-//   });
-// });
 
 app.use('/api/users', userRouter, (req, res)=>{
   res.send(`<h2>Api is running now... </h2>`)
